c7134d51b42b1af37694d1ffa373d53e0d91b97f
mkaganerol97 <mkaganerol97@gmail.com>
2020-08-11 21:57:45 +0300

    mesaj

--- okcuotomasyon/baglanti.cs
+++ okcuotomasyon/baglanti.cs
@@ -11,7 +11,7 @@ namespace okcuotomasyon
     {
         public string connstring = String.Format("Server={0};Port={1};" +
           "User Id={2};Password={3};Database={4};",
-          "localhost", 5432, "postgres", "123456", "okcuotomasyon");
+          "localhost", 5432, "postgres", "123456789", "okcuotomasyon");
 
         public NpgsqlConnection baglan()
         {
